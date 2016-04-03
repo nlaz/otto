0d3a77c46178d6e0b41ee13a1b8e6ad37aaed58e
nlaz <nikolazaris@utexas.edu>
2016-04-03 10:01:19 -0400

    add views

--- nosi_app/settings.py
+++ nosi_app/settings.py
@@ -26,6 +26,9 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_DIRS = (
+  BASE_DIR + '/levels/templates/',
+)
 
 # Application definition
 
@@ -58,8 +61,11 @@ WSGI_APPLICATION = 'nosi_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nosi_app',
+        'USER': 'nlazaris',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- nosi_app/urls.py
+++ nosi_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'nosi_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^levels/', include('levels.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
